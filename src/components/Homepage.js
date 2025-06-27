0b6d5a09ed2576aa3deff703bf88623721cbbc73
ikram <150953076+ikonyx@users.noreply.github.com>
2025-06-27 17:12:04 +0100

    Apply initial styling to homepage layout and elements

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -8,10 +8,14 @@ function homePage() {
     pageContent.appendChild(image);
 
     const textContainer = document.createElement('div');
+    textContainer.id = "txtContainer"
     const header = document.createElement('div');
     header.textContent = "Fine Dining on the Open Sea";
+    header.id = "homeHeader";
+
     const text = document.createElement('div');
     text.textContent = "Step aboard the legendary floating restaurant where culinary excellence meets high-seas adventure. At Baratie, every dish is served with a touch of maritime magic and world-class flavor."
+    text.id = "homeTxt"
     pageContent.appendChild(textContainer);
     textContainer.appendChild(header);
     textContainer.appendChild(text);
